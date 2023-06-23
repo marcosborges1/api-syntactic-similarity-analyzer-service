94b4cdb9d5e23d8cfa2ab515ef94b8e8756b4dbc
marcosborges1 <borges.marcos1@gmail.com>
2023-06-23 14:29:26 -0300

    CompressionBased Class was created

--- string_based/__init__.py
+++ string_based/__init__.py
@@ -4,3 +4,4 @@ from .string_based import StringBased
 from .edit_based import EditBased
 from .token_based import TokenBased
 from .sequence_based import SequenceBased
+from .compression_based import CompressionBased
