dea4bc40ea2e45ec08cf38de875e2d01a8a39621
marcosborges1 <borges.marcos1@gmail.com>
2023-06-27 00:29:25 -0300

    Updated String Based with  new methods

--- string_based/string_based.py
+++ string_based/string_based.py
@@ -7,16 +7,20 @@ class StringBased:
     def __init__(self, name):
         self.name = name
 
-    @abstractmethod
-    def analyze(self):
-        pass
+    def analyze(self, word_list):
+        raise NotImplementedError(
+            "additional_processing must be overridden in subclasses"
+        )
 
-    def to_string(self, results):
+    def get_dataframe(self, results):
         columns = ["word1", "word2"] + self.similarity_measures
         df = pd.DataFrame(results, columns=columns)
+        return df
+
+    def to_string(self, results):
+        df = self.get_dataframe(results)
         print(df.to_string(index=False))
 
     def display_html(self, results):
-        columns = ["word1", "word2"] + self.similarity_measures
-        df = pd.DataFrame(results, columns=columns)
+        df = self.get_dataframe(results)
         display(HTML(df.to_html(index=False)))
