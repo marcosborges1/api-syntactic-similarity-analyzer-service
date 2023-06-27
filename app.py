2d831dff50efc9538e765cc4a125ce807fbde57f
marcosborges1 <borges.marcos1@gmail.com>
2023-06-27 00:31:50 -0300

    Some tests in app

--- app.py
+++ app.py
@@ -1,11 +1,40 @@
-from string_based import TokenBased
+from utils import Utils
+from string_based import (
+    EditBased,
+    TokenBased,
+    SequenceBased,
+    CompressionBased,
+    PhoneticBased,
+    StringBased,
+)
 
-word_list = [["car", "cat"], ["car", "autombile"], ["apple", "orange"], ["id", "badge"]]
+word_list = [
+    ["car", "cat"],
+    ["car", "autombile"],
+    ["apple", "orange"],
+    ["id", "badge"],
+]
+
 
 # edit_based = EditBased("EditBased")
 # results = edit_based.analyze(word_list)
-# print(edit_based.to_string(results))
+# edit_based.to_string(results)
 
-edit_based = TokenBased("token")
-results = edit_based.analyze(word_list)
-print(edit_based.to_string(results))
+sb = EditBased("token")
+token_results = sb.analyze(word_list)
+sb.to_string(token_results)
+
+# df1 = token_based.get_dataframe(token_results)
+
+# edit_based = EditBased("edit")
+# edit_results = edit_based.analyze(word_list)
+# df2 = edit_based.get_dataframe(edit_results)
+
+
+# List of DataFrames
+# dataframes = [df1, df2]
+
+# merged_pdf = Utils.merge_dataframes(
+#     dataframes=dataframes, on_columns=["word1", "word2"]
+# )
+# print(merged_pdf)
