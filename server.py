266eb6c31adbd0940522956c737b4e967ba7198c
marcosborges1 <borges.marcos1@gmail.com>
2023-09-15 19:39:52 -0300

    Set the port from dotenv

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 # server.py
+import os
 import uvicorn
+from dotenv import load_dotenv
 from ariadne import QueryType, make_executable_schema
 from ariadne.asgi import GraphQL
 from my_graphql.schemas.index import type_defs
 from my_graphql.resolvers.index import resolve_get_dummy_data
 
+# Load dotenv
+load_dotenv()
 query = QueryType()
 query.set_field("getDummyData", resolve_get_dummy_data)
 
@@ -12,4 +16,4 @@ schema = make_executable_schema(type_defs, query)
 app = GraphQL(schema, debug=True)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=4003)
+    uvicorn.run(app, host="localhost", port=os.getenv("PORT"))
