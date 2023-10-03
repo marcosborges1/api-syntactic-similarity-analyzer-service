e907ef5760530bd2029f15956de06b1bf335167e
marcosborges1 <borges.marcos1@gmail.com>
2023-10-03 12:31:31 -0300

    Server updated for auto reload

--- server.py
+++ server.py
@@ -2,10 +2,11 @@
 import os
 import uvicorn
 from dotenv import load_dotenv
-from ariadne import QueryType, make_executable_schema
+from ariadne import QueryType
 from ariadne.asgi import GraphQL
 from my_graphql.schemas.index import type_defs
 from my_graphql.resolvers.index import resolve_syntactic_similarities,resolve_api_intergration_points
+from ariadne.contrib.federation import make_federated_schema
 
 # Load dotenv
 load_dotenv()
@@ -13,8 +14,9 @@ query = QueryType()
 query.set_field("getSyntacticSimilarities", resolve_syntactic_similarities)
 query.set_field("getAPIIntegrationPoints", resolve_api_intergration_points)
 
-schema = make_executable_schema(type_defs, query)
+schema = make_federated_schema(type_defs, query)
 app = GraphQL(schema, debug=True)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=os.getenv("PORT"))
+    # uvicorn.run(app, host="localhost", port=int(os.getenv("PORT")))
+    uvicorn.run("server:app", host="localhost", port=int(os.getenv('PORT')), reload=True)
